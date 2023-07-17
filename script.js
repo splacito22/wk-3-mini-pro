2feedabd7fee22839078e0373a741590f235a1cb
Stephany <136421961+splacito22@users.noreply.github.com>
2023-07-16 22:12:39 -0700

    added files to repo, pushed and pulled, git first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var gon = {
+  choices: ["r", "p", "s"],
+};
+
+var choice = prompt("Select one of the following choices: r, p ,s");
+
+alert();
+
+function game() {
+  prompt("Select one of the following choices: r, p ,s");
+}
+
+game();
