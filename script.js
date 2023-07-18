afbc96538345b442e0fda2f48adc5a49ffa72ea1
Stephany <136421961+splacito22@users.noreply.github.com>
2023-07-17 22:46:21 -0700

    first implementation finished game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
 var ties = 0;
+var messageEl = document.querySelector("#message");
 
 var options = ["R", "P", "S"];
 function playGame() {
@@ -8,6 +9,7 @@ function playGame() {
 
   if (!userChoice) {
     console.log("Game ended.");
+    messageEl.textContent = "The game was ended. Have a nice day!";
     return;
   }
 
@@ -15,6 +17,33 @@ function playGame() {
   var computerChoice = options[Math.floor(Math.random() * options.length)];
 
   window.alert("The computer chose " + computerChoice);
+
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!");
+  } else {
+    losses++;
+    window.alert("You lost!");
+  }
+
+  window.alert(
+    "Stats: Wins: " + wins + " Losses: " + losses + " Ties: " + ties
+  );
+
+  var playAgain = window.confirm("Play again?");
+
+  if (playAgain) {
+    playGame();
+  }
+
+  messageEl.textContent = "Thank you for playing!";
 }
 
 playGame();
